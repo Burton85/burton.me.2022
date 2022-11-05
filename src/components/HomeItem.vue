38201a27af9f7a3f095d5c508ccb782e7714ffda
BurtonZH Hou <burtonzh.hou@ogilvy.com>
2022-11-05 23:55:29 +0800

    update

--- src/components/HomeItem.vue
+++ src/components/HomeItem.vue
@@ -13,31 +13,15 @@
 </template>
 <script>
 export default {
-  // props: {
-  //     homeItemList(){
-  //         return{
-  //             type: Object,
-  //             default: { name: 'nodata', checkItems: [{ name: 'nodata' }, { name: 'nodata' }, { name: 'nodata' }] },
-  //         }
-  //     },
-  //     index(){
-  //         return{
-  //             type: String,
-  //             default: '0',
-  //         }
-  //     },
-  //     type: {
-  //         type: String,
-  //         default: 'note',
-  //     },
-  // },
   props: ['homeItemList', 'index', 'type'],
   methods: {
     goNext() {
-      this.$router.push({
-        path: '/' + this.type + 'detail',
-        query: { id: this.index }
-      });
+      if (this.type !== 'profile') {
+        this.$router.push({
+          path: '/' + this.type + 'detail',
+          query: { id: this.index }
+        });
+      }
     }
   }
 };
--- src/router.js
+++ src/router.js
@@ -1,20 +1,56 @@
-import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-export default function (type) {
-    const routerHistory = type === 'client' ? createWebHistory() : createMemoryHistory();
+export default function () {
+  const routerHistory = createWebHashHistory();
 
-    return createRouter({
-        history: routerHistory,
-        routes: [
-            { path: '/', component: () => import('./views/HomePage.vue'), props: true },
-            { path: '/note', component: () => import('./views/SubPage.vue'), props: true },
-            { path: '/photo', component: () => import('./views/SubPage.vue'), props: true },
-            { path: '/project', component: () => import('./views/SubPage.vue'), props: true },
-            { path: '/profile', component: () => import('./views/SubPage.vue'), props: true },
-            { path: '/notedetail', component: () => import('./views/NoteDetail.vue'), props: true },
-            { path: '/projectdetail', component: () => import('./views/ProjectDetail.vue'), props: true },
-            { path: '/photodetail', component: () => import('./views/PhotoDetail.vue'), props: true },
-            { path: '/profiledetail', component: () => import('./views/ProfileDetail.vue'), props: true },
-        ],
-    });
+  return createRouter({
+    history: routerHistory,
+    routes: [
+      {
+        path: '/',
+        component: () => import('./views/HomePage.vue'),
+        props: true
+      },
+      {
+        path: '/note',
+        component: () => import('./views/SubPage.vue'),
+        props: true
+      },
+      {
+        path: '/photo',
+        component: () => import('./views/SubPage.vue'),
+        props: true
+      },
+      {
+        path: '/project',
+        component: () => import('./views/SubPage.vue'),
+        props: true
+      },
+      {
+        path: '/profile',
+        component: () => import('./views/SubPage.vue'),
+        props: true
+      },
+      {
+        path: '/notedetail',
+        component: () => import('./views/NoteDetail.vue'),
+        props: true
+      },
+      {
+        path: '/projectdetail',
+        component: () => import('./views/ProjectDetail.vue'),
+        props: true
+      },
+      {
+        path: '/photodetail',
+        component: () => import('./views/PhotoDetail.vue'),
+        props: true
+      },
+      {
+        path: '/profiledetail',
+        component: () => import('./views/ProfileDetail.vue'),
+        props: true
+      }
+    ]
+  });
 }
