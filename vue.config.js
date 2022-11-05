1d3299374d52daa97f733570869ed02cd35ba869
BurtonZH Hou <burtonzh.hou@ogilvy.com>
2022-11-06 00:38:29 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './', // 打包時預設的相對路徑
-  outputDir: 'dist', // 打包的目錄名稱
+  outputDir: 'docs', // 打包的目錄名稱
   filenameHashing: true,
   devServer: {
     // 開發環境下將 API 請求代理到 API 伺服器
@@ -17,4 +17,4 @@ module.exports = defineConfig({
   css: {
     extract: true //生產環境下是 true，開發環境下是 false // 是否將元件中的 CSS 提取至一個獨立的 CSS 檔案中 (而不是動態注入到 JavaScript中的inline程式碼)
   }
-})
+});
